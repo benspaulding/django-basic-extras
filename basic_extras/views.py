d955b6d9df8d07bbf2201f85424b3cedbbbe1244
Ben Spaulding <ben@benspaulding.us>
2012-10-22 20:47:56 -0500

    Add PlainTextTemplateView.

---
+++ basic_extras/views.py
@@ -0,0 +1,10 @@
+from django.views.generic.base import TemplateView
+
+
+class PlainTextTemplateView(TemplateView):
+    """ A plain text generic template view. """
+
+    def render_to_response(self, context, **kwargs):
+        return super(PlainTextTemplateView, self).render_to_response(
+            context, content_type='text/plain', **kwargs
+            )
