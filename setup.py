1e97277fcbaa7c78354d98cb779840ff388fd438
Ben Spaulding <ben@benspaulding.us>
2012-11-12 21:07:03 -0600

    Fix formatting of setup.py.

--- setup.py
+++ setup.py
@@ -1,34 +1,32 @@
 # -*- coding: utf-8 -*-
 
-import os
-
 from distutils.core import setup
 
 
 version = __import__('basic_extras').__version__
 
 setup(
-    name = 'django-basic-extras',
-    version = version,
-    description = 'A small collection of some oft-used Django bits.',
-    url = 'https://github.com/benspaulding/django-basic-extras/',
-    author = 'Ben Spaulding',
-    author_email = 'ben@benspaulding.us',
-    license = 'BSD',
-    long_description = open('README.rst').read(),
-    requires = ['Django (>=1.3.1)'],
-    packages = [
+    name='django-basic-extras',
+    version=version,
+    description='A small collection of some oft-used Django bits.',
+    url='https://github.com/benspaulding/django-basic-extras/',
+    author='Ben Spaulding',
+    author_email='ben@benspaulding.us',
+    license='BSD',
+    long_description=open('README.rst').read(),
+    requires=['Django (>=1.3.1)'],
+    packages=[
         'basic_extras',
         'basic_extras.templatetags',
         'basic_extras.tests',
     ],
-    package_data = {
+    package_data={
         'basic_extras': [
             'locale/*/LC_MESSAGES/*',
         ],
     },
-    platforms = ['Any'],
-    classifiers = [
+    platforms=['Any'],
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Django',
